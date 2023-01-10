45122d9760d3b6299495870a0260c40f0ae52cc6
Michael Garbuzov <michaelgarbuzov1994@gmail.com>
2023-01-10 14:52:55 +0200

    update remove and done todos

--- src/components/form.js
+++ src/components/form.js
@@ -26,13 +26,6 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
     </form>
   );
 };
