6d92622585939bb0fd24722100049d086d4bd24b
Michael Garbuzov <michaelgarbuzov1994@gmail.com>
2023-01-10 11:29:53 +0200

    update form and to do list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div>App</div>
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
+    </div>
   );
 }
 
